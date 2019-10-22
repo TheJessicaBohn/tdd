6a8c4ee609f73788dbace1057026e12ed4aa8ea4
jessicabohn74 <47541659+jessicabohn74@users.noreply.github.com>
2019-10-21 22:33:35 -0300

    Add files via upload

---
+++ galaoconversao/ConversaoGalaoLitro/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ConversaoGalaoLitro.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConversaoGalaoLitro.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var thamirys = "Gameiro";
+            return View();
+        }
+        // Renato está na Unip
+
+        // Renato Groffe: não sou um bot
+        // Teste 123
+
+        [HttpPost]
+        public ActionResult Index(FormCollection frm)
+        {
+            var valorGalao = Convert.ToDouble(frm["txtGalao"]);
+
+            Console.WriteLine("Palmeiras nao tem mundial");
+
+            var conversao = new Conversao();
+            var resultadoConversao = conversao.GalaoParaLitros(valorGalao);
+            ViewBag.ResultadoConversao = resultadoConversao;
+
+            return View();
+        }
+    }
+}
